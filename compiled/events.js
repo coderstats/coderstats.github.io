cec0d7f9e72e63ee5f0e7ccbc727b07cf3612768
Ramiro Gómez <code@ramiro.org>
2019-05-26 01:20:06 +0200

    new deployment

--- compiled/events.js
+++ compiled/events.js
@@ -1,1 +1,25 @@
-let events=new Vue({el:'#events',data:{events:null,response:null},computed:{users:function(){return d3.nest().key((a)=>a.actor.login).entries(this.events.filter((a)=>'PushEvent'===a.type)).sort((c,a)=>a.values.length-c.values.length)}},created:function(){this.fetchEvents()},methods:{fetchEvents:function(){this.$http.get('https://api.github.com/events').then((a)=>{this.response=a,this.events=a.body})}}});
+let events = new Vue({
+    el: '#events',
+    data: {
+        events: null,
+        response: null
+    },
+    computed: {
+        users: function () {
+            return d3.nest().key(d => d.actor.login).entries(this.events.filter(d => d.type === 'PushEvent')).sort((a, b) => b.values.length - a.values.length);
+        }
+    },
+    created: function () {
+        this.fetchEvents();
+    },
+    methods: {
+        fetchEvents: function () {
+            let url = 'https://api.github.com/events';
+            //url = '/data/events.json';
+            this.$http.get(url).then(response => {
+                this.response = response;
+                this.events = response.body;
+            });
+        }
+    }
+});
