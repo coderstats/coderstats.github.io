82b0e1e9d3476f2212b95655a19393efb39432bf
Ramiro Gómez <code@ramiro.org>
2019-05-29 11:17:19 +0200

    new deployment

--- compiled/front.js
+++ compiled/front.js
@@ -1,8 +1,59 @@
-Vue.component('user-link',{props:['login','avatar_url'],template:`
+Vue.component('user-link', {
+    props: ['login', 'avatar_url'],
+    template: `
     <div class="col-1-xl col-2-l col-4-m col-12">
         <a v-bind:href="'/github#' + login">
             <img :src="avatar_url" :alt="login"/><br>
             <i class="fa fa-user" aria-hidden="true"></i> {{ login }}
         </a>
     </div>
-    `}),new Vue({el:'#active-users',data:{events:null},computed:{users:function(){return d3.nest().key((a)=>a.actor.login).entries(this.events.filter((a)=>'PushEvent'===a.type)).sort((c,a)=>a.values.length-c.values.length)}},created:function(){this.$http.get('https://api.github.com/events').then((a)=>{this.events=a.body})}}),new Vue({el:'#followed-users',data:{users:[]},created:function(){this.$http.get('/data/users.json').then((a)=>{this.users=a.body.items})}}),new Vue({el:'#most-repos-users',data:{users:[]},created:function(){this.$http.get('/data/most-repos-users.json').then((a)=>{this.users=a.body.items})}}),new Vue({el:'#earliest-users',data:{users:[]},created:function(){this.$http.get('/data/earliest-users.json').then((a)=>{this.users=a.body.items})}});
+    `
+});
+
+new Vue({
+    el: '#active-users',
+    data: { events: null },
+    computed: {
+        users: function () {
+            return d3.nest().key(d => d.actor.login).entries(this.events.filter(d => d.type === 'PushEvent')).sort((a, b) => b.values.length - a.values.length);
+        }
+    },
+    created: function () {
+        this.$http.get('https://api.github.com/events').then(response => {
+            this.events = response.body;
+        });
+    }
+});
+
+new Vue({
+    el: '#followed-users',
+    data: { users: [] },
+    created: function () {
+        // https://api.github.com/search/users?q=repos:%3E1&sort=followers&order=desc
+        this.$http.get('/data/users.json').then(response => {
+            this.users = response.body.items;
+        });
+    }
+});
+
+new Vue({
+    el: '#most-repos-users',
+    data: { users: [] },
+    created: function () {
+        // https://api.github.com/search/users?q=repos:%3E1%20type:user&sort=repositories&order=desc
+        this.$http.get('/data/most-repos-users.json').then(response => {
+            this.users = response.body.items;
+        });
+    }
+});
+
+new Vue({
+    el: '#earliest-users',
+    data: { users: [] },
+    created: function () {
+        // https://api.github.com/search/users?q=repos:%3E1%20type:user&sort=joined&order=asc
+        this.$http.get('/data/earliest-users.json').then(response => {
+            this.users = response.body.items;
+        });
+    }
+});
